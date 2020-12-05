739ccc8dc0a044397c7fb6aebf2f1a1c1163d144
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2020-12-05 08:42:56 -0500

    Basic starting point

---
+++ api/Models/IndexerWorkflow/Correlate/CorrelationModel.cs
@@ -0,0 +1,11 @@
+﻿using LiteralLifeChurch.ArchiveManagerApi.Models.IndexerWorkflow.Extract;
+
+namespace LiteralLifeChurch.ArchiveManagerApi.Models.IndexerWorkflow.Correlate
+{
+    public class CorrelationModel : IIndexerWorkflowModel
+    {
+        public DateModel Date { get; set; }
+
+        public NameModel Name { get; set; }
+    }
+}
